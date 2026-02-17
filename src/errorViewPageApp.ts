6286ada2faf1efb0385b074e9138f65b56be41da
sofiso99 <sofrodasg@gmail.com>
2026-02-17 20:20:39 +0100

    main updated

--- src/errorViewPageApp.ts
+++
@@ -1,5 +0,0 @@
-//Webview file: (<webview-name>PageApp.ts): This file manages the Webview content. In this file you should:
-
-//Interact with DOM elements within the HTML.
-
-//Add event listeners and manipulate HTML elements, excluding vscode and workspace elements.
